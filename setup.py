a3d3e74aea609c66b640ea6d9ac0d821ade62e5a
B-enguin <e0959432@u.nus.edu>
2025-04-11 01:39:42 +0200

    Change package name.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import os
 os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    name="speedy_diff_gaussian_rasterization",
+    name="diff_gaussian_rasterization",
     packages=['diff_gaussian_rasterization'],
     ext_modules=[
         CUDAExtension(
