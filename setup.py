b78b43e6dfd89d26bba65d800c7a253ea1a829ed
B-enguin <e0959432@u.nus.edu>
2025-04-11 00:46:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import os
 os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    name="diff_gaussian_rasterization",
+    name="speedy_diff_gaussian_rasterization",
     packages=['diff_gaussian_rasterization'],
     ext_modules=[
         CUDAExtension(
